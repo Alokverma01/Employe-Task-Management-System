2a15d14f45cb1c0b37248f640c0c96069b4aaaf0
Alokverma01 <alokv5661@gmail.com>
2025-01-27 00:11:36 -1200

    change in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline ">jai shree ram</h1>
-        <h2 className='text-amber-500 text-3xl'>jai bajranbali</h2>
+       
       </div>
     </>
   )
