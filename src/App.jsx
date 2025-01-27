f6c87af401ea8d026098e07acbd8590a011d73a4
Alokverma01 <alokv5661@gmail.com>
2025-01-27 00:05:39 -1200

    project started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline ">jai shree ram</h1>
+        <h2 className='text-amber-500 text-3xl'>jai bajranbali</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
